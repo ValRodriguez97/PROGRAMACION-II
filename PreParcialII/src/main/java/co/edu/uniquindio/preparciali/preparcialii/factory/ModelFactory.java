ee54d3c0209f00647a813d86479b7ec3ee9068b8
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2024-10-18 19:26:32 -0500

    aplicacion en el model factory

--- PreParcialII/src/main/java/co/edu/uniquindio/preparciali/preparcialii/factory/ModelFactory.java
+++ PreParcialII/src/main/java/co/edu/uniquindio/preparciali/preparcialii/factory/ModelFactory.java
@@ -1,4 +1,40 @@
 package co.edu.uniquindio.preparciali.preparcialii.factory;
 
-public class ModelFactory implements {
+import co.edu.uniquindio.preparciali.preparcialii.mapping.dto.ClienteDto;
+import co.edu.uniquindio.preparciali.preparcialii.mapping.mappers.PrestamoUQMappingImplt;
+import co.edu.uniquindio.preparciali.preparcialii.model.PrestamoUQ;
+import co.edu.uniquindio.preparciali.preparcialii.service.IModelFactoryService;
+
+import java.util.List;
+
+public class ModelFactory implements IModelFactoryService {
+    private static ModelFactory instance;
+    PrestamoUQ prestamoUQ;
+    PrestamoUQMappingImplt mapper;
+
+    public static ModelFactory getInstance() {
+        if (instance == null) {
+            instance = new ModelFactory();
+        }
+        return instance;
+    }
+
+    private ModelFactory() {
+        mapper = new PrestamoUQMappingImplt();
+       prestamoUQ = inicializarDatos();
+    }
+
+    public PrestamoUQ getPrestamoUQ() {
+        return prestamoUQ;
+    }
+
+    @Override
+    public List<ClienteDto> getClientes(){
+        return mapper.getClientesDto(prestamoUQ.getListaClientes());
+    }
+
+    public static PrestamoUQ inicializarDatos() {
+        PrestamoUQ prestamoUQ = new PrestamoUQ("Prestamos");
+        return prestamoUQ;
+    }
 }
